6110fc3bebf78096102b211482eadf51c585f1ff
Edmar Paulino <edmarpaulino9@gmail.com>
2021-11-21 12:36:54 -0300

    fix norm error

--- src/stk/stack_destroy.c
+++ src/stk/stack_destroy.c
@@ -6,7 +6,7 @@
 /*   By: edpaulin <edpaulin@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/21 12:15:41 by edpaulin          #+#    #+#             */
-/*   Updated: 2021/11/21 12:20:40 by edpaulin         ###   ########.fr       */
+/*   Updated: 2021/11/21 12:34:35 by edpaulin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	stack_destroy(t_stack *stack_ref)
 	t_stack			*stack;
 	t_stack_node	*node;
 	t_stack_node	*temp;
-	
+
 	if (stack_ref)
 	{
 		stack = stack_ref;
