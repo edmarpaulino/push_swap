0b41819983ae99d2c68194a9d45c4386be3fb50f
Edmar Paulino <edmarpaulino9@gmail.com>
2021-11-21 11:58:57 -0300

    add stack header file and data structs

--- inc/push_swap.h
+++ inc/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: edpaulin <edpaulin@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/04 17:37:32 by edpaulin          #+#    #+#             */
-/*   Updated: 2021/11/21 11:22:19 by edpaulin         ###   ########.fr       */
+/*   Updated: 2021/11/21 11:52:16 by edpaulin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,17 @@
 # define PUSH_SWAP_H
 
 # include "libft.h"
+# include "stack.h"
 
 # define MIN_INT -2147483648
 # define MAX_INT 2147483647
+
+typedef struct s_data
+{
+	t_stack	*stack_a;
+	t_stack	*stack_b;
+}				t_data;
+
 
 void	check_start_arguments(const int argc, const char **argv);
 
--- inc/stack.h
+++ inc/stack.h
@@ -1,19 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   stack.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: edpaulin <edpaulin@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/04 17:34:53 by edpaulin          #+#    #+#             */
-/*   Updated: 2021/11/21 11:27:14 by edpaulin         ###   ########.fr       */
+/*   Created: 2021/11/21 11:29:17 by edpaulin          #+#    #+#             */
+/*   Updated: 2021/11/21 11:54:39 by edpaulin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "push_swap.h"
+#ifndef STACK_H
+# define STACK_H
 
-int	main(int argc, char **argv)
+# include "libft.h"
+
+typedef struct s_stack_node
 {
-	check_start_arguments(argc, (const char **)argv);
-	return (0);
-}
+	int					content;
+	struct s_stack_node	*next;
+	struct s_stack_node	*prev;
+}				t_stack_node;
+
+typedef struct s_stack
+{
+	t_stack_node	*top;
+	t_stack_node	*begin;
+	int				size;
+}				t_stack;
+
+#endif
