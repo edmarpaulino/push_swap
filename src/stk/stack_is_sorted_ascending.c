4ba08ce3a35b16209949e6d7e32bc4b5693b60cb
Edmar Paulino <edmarpaulino9@gmail.com>
2021-11-23 12:02:28 -0300

    rename stack_is_sorted to stack_is_sorted_ascending

--- src/stk/stack_is_sorted_ascending.c
+++ src/stk/stack_is_sorted_ascending.c
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   stack_is_sorted.c                                  :+:      :+:    :+:   */
+/*   stack_is_sorted_ascending.c                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: edpaulin <edpaulin@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/22 14:10:29 by edpaulin          #+#    #+#             */
-/*   Updated: 2021/11/22 14:13:30 by edpaulin         ###   ########.fr       */
+/*   Updated: 2021/11/23 11:59:56 by edpaulin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "stack.h"
 
-int	stack_is_sorted(t_stack *stack_ref)
+int	stack_is_sorted_ascending(t_stack *stack_ref)
 {
 	t_stack			*stack;
 	t_stack_node	*node;
