a07d7970c99ca2cb3fd3aa465a45587b01519b2a
Edmar Paulino <edmarpaulino9@gmail.com>
2021-11-26 14:40:35 -0300

    fix complex_sort to send only the smallest values

--- src/complex_sort.c
+++ src/complex_sort.c
@@ -6,7 +6,7 @@
 /*   By: edpaulin <edpaulin@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/26 14:18:30 by edpaulin          #+#    #+#             */
-/*   Updated: 2021/11/26 14:33:15 by edpaulin         ###   ########.fr       */
+/*   Updated: 2021/11/26 14:39:11 by edpaulin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ static void	split_the_stack(t_stack *stack_src_ref, t_stack *stack_dest_ref)
 	half = (stack_src_ref->size / 2);
 	while (stack_src_ref->size > half)
 	{
-		if (stack_src_ref->top->content > pivot)
+		if (stack_src_ref->top->content < pivot)
 			push(stack_src_ref, stack_dest_ref);
 		else
 			rotate(stack_src_ref);
