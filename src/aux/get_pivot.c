019af655bcf97f90d455812ab0a7161ccfba54e5
Edmar Paulino <edmarpaulino9@gmail.com>
2021-12-06 10:07:36 -0300

    refactoring get_pivot

--- src/aux/get_pivot.c
+++ src/aux/get_pivot.c
@@ -6,98 +6,58 @@
 /*   By: edpaulin <edpaulin@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 16:02:41 by edpaulin          #+#    #+#             */
-/*   Updated: 2021/12/04 21:05:57 by edpaulin         ###   ########.fr       */
+/*   Updated: 2021/12/06 10:06:43 by edpaulin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static void	swap2(int *list, int j, int i)
+static int	*get_array(t_stack *block_ref, int len)
 {
-	int	aux;
-
-	aux = list[j];
-	list[j] = list[i];
-	list[i] = aux;
-}
-
-static int	partition(int *list, int begin, int end)
-{
-	int	pivot;
-	int	i;
-	int	j;
-
-	pivot = list[end];
-	i = begin;
-	j = begin;
-	while (j < end)
-	{
-		if (list[j] <= pivot)
-		{
-			swap2(list, j, i);
-			i++;
-		}
-		j++;
-	}
-	swap2(list, i, end);
-	return (i);
-}
-
-static void	quicksort(int *list, int begin, int end)
-{
-	int	pivot;
-
-	if (begin < end)
-	{
-		pivot = partition(list, begin, end);
-		quicksort(list, begin, (pivot - 1));
-		quicksort(list, (pivot + 1), end);
-	}
-}
-
-static int	*get_list(t_stack *block_ref, int len)
-{
-	int				*list;
 	int				index;
+	int				*array;
 	t_stack_node	*block_node;
 
-	list = malloc(len * sizeof(int));
-	if (!list)
-		return (NULL);
-	block_node = block_ref->top;
-	index = 0;
-	while (block_node && index < len)
+	array = malloc(len * sizeof(int));
+	if (array)
 	{
-		list[index] = block_node->content;
-		block_node = block_node->prev;
-		index++;
+		block_node = block_ref->top;
+		index = 0;
+		while (block_node && index < len)
+		{
+			array[index] = block_node->content;
+			block_node = block_node->prev;
+			index++;
+		}
 	}
-	return (list);
+	return (array);
 }
 
 int	get_pivot(t_stack *block_ref, int len, int flag)
 {
 	int	pivot;
-	int	*temp;
+	int	*array;
 
-	temp = get_list(block_ref, len);
-	if (!temp)
-		return (0);
-	quicksort(temp, 0, (len - 1));
-	if (flag == 0)
+	pivot = 0;
+	array = get_array(block_ref, len);
+	if (array)
 	{
-		if ((len % 2) == 0)
-			pivot = temp[(len / 2)];
+		ft_quicksort(array, 0, (len - 1));
+		if (flag == 0)
+		{
+			if ((len % 2) == 0)
+				pivot = array[(len / 2)];
+			else
+				pivot = array[(len / 2) + 1];
+		}
 		else
-			pivot = temp[(len / 2) + 1];
+		{
+			if ((len % 2) == 0)
+				pivot = array[(len / 2) - 1];
+			else
+				pivot = array[(len / 2)];
+		}
+		free(array);
 	}
-	else
-	{
-		if ((len % 2) == 0)
-			pivot = temp[(len / 2) - 1];
-		else
-			pivot = temp[(len / 2)];
-	}
-	free(temp);
 	return (pivot);
 }
